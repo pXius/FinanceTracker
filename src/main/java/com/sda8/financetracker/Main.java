9038eeb68f1cd3d4840a170dbde836360e60e67e
pXius <fan.engelbrecht@gmail.com>
2020-10-16 20:47:46 +0200

    Main method and tracker core class Javadoc updates

--- src/main/java/com/sda8/financetracker/Main.java
+++ src/main/java/com/sda8/financetracker/Main.java
@@ -2,6 +2,10 @@ package com.sda8.financetracker;
 
 import com.sda8.financetracker.app.TrackerApp;
 
+/**
+ * This class serves as an entry point to the tracker app and nothing else.
+ * A TrackerApp instance in instantiated and it's run() method is called.
+ */
 public class Main {
 
     public static void main(String[] args) {
